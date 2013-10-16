7a1433358cd5cf7fb854eb158212df54b7c9de2c
kalle saas <kalle@easypep.de>
2013-10-16 11:45:13 +0200

    user more descriptive names to set instance variables

--- lib/fastbill-automatic/webhook.rb
+++ lib/fastbill-automatic/webhook.rb
@@ -15,11 +15,11 @@ module Fastbill
       def set_attributes(attributes)
         attributes.each_pair do |key, value|
           if key == "customer"
-            instance_variable_set("@#{key.downcase}", Customer.new(value))
+            instance_variable_set("@customer", Customer.new(value))
           elsif key == "subscription"
-            instance_variable_set("@#{key.downcase}", Subscription.new(value))
+            instance_variable_set("@subscription", Subscription.new(value))
           elsif key == "payment"
-            instance_variable_set("@#{key.downcase}", Payment.new(value))
+            instance_variable_set("@payment", Payment.new(value))
           else
             instance_variable_set("@#{key.downcase}", value)
           end
