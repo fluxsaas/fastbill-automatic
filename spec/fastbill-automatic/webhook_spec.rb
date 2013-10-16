1e54b22a0566ae3aad8c7e9ed9e3a53f9a82d4a0
kalle saas <kalle@easypep.de>
2013-10-16 11:45:25 +0200

    use demo data

--- spec/fastbill-automatic/webhook_spec.rb
+++ spec/fastbill-automatic/webhook_spec.rb
@@ -7,27 +7,27 @@ describe Fastbill::Automatic::Webhook do
       id:44140,
       type:"subscription.created",
       customer:{
-        customer_id:"321862",
-        customer_ext_uid:"6191",
-        hash:"a2c1e189660f611af2e1df219a128645",
+        customer_id:"2131231231",
+        customer_ext_uid:"2321412343",
+        hash:"some-secret-hash",
         customer_number:"21",
-        companyname:"JimFish",
+        companyname:"Demo Data GmbH",
         salutation:"",
-        firstname:"kalle",
-        lastname:"Saas",
-        address:"Schulterblatt 10",
+        firstname:"Ben",
+        lastname:"Stone",
+        address:"Demostr. 123",
         address_2:"",
-        zipcode:"20357",
+        zipcode:"12345",
         city:"Hamburg",
         country_code:"DE",
-        email:"admin@easypep.de",
-        payment_data_url:"https:\/\/automatic.fastbill.com\/accountdata\/7ccbc6b2183eb16ea87542ce5a8048e4\/a2c1e189660f611af2e1df219a128645",
-        dashboard_url:"https:\/\/automatic.fastbill.com\/dashboard\/7ccbc6b2183eb16ea87542ce5a8048e4\/a2c1e189660f611af2e1df219a128645"
+        email:"developers@fastbill.com",
+        payment_data_url:"https:\/\/automatic.fastbill.com\/accountdata\/some-secret-hash\/some-secret-hash",
+        dashboard_url:"https:\/\/automatic.fastbill.com\/dashboard\/some-secret-hash\/some-secret-hash"
       },
       subscription:{
-        subscription_id:14290,
+        subscription_id:123123,
         subscription_ext_uid:"",
-        hash:"892ec0d09217aca514cd85b9d78047c0",
+        hash:"some-secret-hash",
         article_code:"basic_1_9",
         start_date:"2013-10-14 09:35:54",
         last_event:"2013-10-14 09:35:54",
@@ -36,6 +36,43 @@ describe Fastbill::Automatic::Webhook do
         status:"active"
       },
       created:"2013-10-14 09:35:56"
+    }.to_json
+  end
+
+  let!(:subscription_changed) do
+    {
+      id: 44722,
+      type: "subscription.changed",
+      customer: {
+        customer_id: "322082",
+        customer_ext_uid: "9655",
+        hash: "some-secret-hash",
+        customer_number: "22",
+        companyname: "Demo Data GmbH",
+        salutation: "",
+        firstname: "Ben",
+        lastname: "Stone",
+        address: "Demostr. 123",
+        address_2: "",
+        zipcode: "12345",
+        city: "Hamburg",
+        country_code: "DE",
+        email: "developers@fastbill.com",
+        payment_data_url: "https:\/\/automatic.fastbill.com\/accountdata\/some-secret-hash\/some-secret-hash",
+        dashboard_url:"https: \/\/automatic.fastbill.com\/dashboard\/some-secret-hash\/some-secret-hash"
+      },
+      subscription: {
+        subscription_id: 353,
+        subscription_ext_uid: "",
+        hash: "some-secret-hash",
+        article_code: "basic_6_9",
+        start_date: "2013-07-02 18:59:18",
+        last_event: "2013-10-14 16:02:41",
+        next_event: "2014-04-14 16:02:41",
+        cancellation_date: "0000-00-00 00:00:00",
+        status: "active"
+      },
+      created: "2013-10-14 16:03:02"
     }.to_json
   end
 
@@ -48,16 +85,16 @@ describe Fastbill::Automatic::Webhook do
         customer_ext_uid: "9655",
         hash: "some-secret-hash",
         customer_number: "22",
-        companyname: "bto",
+        companyname: "Demo Data GmbH",
         salutation: "",
-        firstname: "kalle",
-        lastname: "Saas",
-        address: "Schulterblatt 10",
+        firstname: "Ben",
+        lastname: "Stone",
+        address: "Demostr. 123",
         address_2: "",
-        zipcode: "20357",
+        zipcode: "12345",
         city: "Hamburg",
         country_code: "DE",
-        email: "admin@easypep.de",
+        email: "developers@fastbill.com",
         payment_data_url: "https: \/\/automatic.fastbill.com\/accountdata\/some-secret-hash\/some-secret-hash",
         dashboard_url: "https: \/\/automatic.fastbill.com\/dashboard\/some-secret-hash\/some-secret-hash"
       },
@@ -81,27 +118,27 @@ describe Fastbill::Automatic::Webhook do
       id:44138,
       type:"payment.created",
       customer:{
-        customer_id:"321862",
-        customer_ext_uid:"6191",
-        hash:"a2c1e189660f611af2e1df219a128645",
+        customer_id:"2131231231",
+        customer_ext_uid:"2321412343",
+        hash:"some-secret-hash",
         customer_number:"21",
-        companyname:"JimFish",
+        companyname:"Demo Data GmbH",
         salutation:"",
-        firstname:"kalle",
-        lastname:"Saas",
-        address:"Schulterblatt 10",
+        firstname:"Ben",
+        lastname:"Stone",
+        address:"Demostr. 123",
         address_2:"",
-        zipcode:"20357",
+        zipcode:"12345",
         city:"Hamburg",
         country_code:"DE",
-        email:"admin@easypep.de",
-        payment_data_url:"https:\/\/automatic.fastbill.com\/accountdata\/7ccbc6b2183eb16ea87542ce5a8048e4\/a2c1e189660f611af2e1df219a128645",
-        dashboard_url:"https:\/\/automatic.fastbill.com\/dashboard\/7ccbc6b2183eb16ea87542ce5a8048e4\/a2c1e189660f611af2e1df219a128645"
+        email:"developers@fastbill.com",
+        payment_data_url:"https:\/\/automatic.fastbill.com\/accountdata\/some-secret-hash\/some-secret-hash",
+        dashboard_url:"https:\/\/automatic.fastbill.com\/dashboard\/some-secret-hash\/some-secret-hash"
       },
       subscription:{
-        subscription_id:14290,
+        subscription_id:123123,
         subscription_ext_uid: nil,
-        hash:"892ec0d09217aca514cd85b9d78047c0",
+        hash:"some-secret-hash",
         article_code:"basic_1_9"
       },
       payment:{
@@ -134,6 +171,26 @@ describe Fastbill::Automatic::Webhook do
 
       it "should have the correct type" do
         webhook.type.should eq "subscription.created"
+      end
+
+      it "should have a customer" do
+        webhook.customer.should be_a Fastbill::Automatic::Customer
+      end
+
+      it "should have a subscription" do
+        webhook.subscription.should be_a Fastbill::Automatic::Subscription
+      end
+
+    end
+
+    describe "#changed" do
+
+      let (:webhook) do
+        Fastbill::Automatic::Webhook.parse(subscription_changed)
+      end
+
+      it "should have the correct type" do
+        webhook.type.should eq "subscription.changed"
       end
 
       it "should have a customer" do
