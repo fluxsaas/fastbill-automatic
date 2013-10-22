a9d6a3c12bcfbed7a2e8a893d8ad4ed7d56b9a10
kalle saas <kalle@easypep.de>
2013-10-22 21:07:06 +0200

    added more attr_accessor

--- lib/fastbill-automatic/subscription.rb
+++ lib/fastbill-automatic/subscription.rb
@@ -9,7 +9,7 @@ module Fastbill
 
       attr_accessor :subscription_id, :customer_id, :subscription_ext_uid, :article_number, :customer_id,
                     :coupon, :title, :unit_price, :currency_code, :next_event, :quantity, :description,
-                    :usage_date, :status, :hash
+                    :usage_date, :status, :hash, :article_code
     end
   end
 end
