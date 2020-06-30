a3f56e2a956c06720a44cf863f12babdd9895a37
kalle saas <kalle@easypep.de>
2020-06-30 16:15:56 +0200

    add Update to article

--- lib/fastbill-automatic/article.rb
+++ lib/fastbill-automatic/article.rb
@@ -1,10 +1,25 @@
 module Fastbill
   module Automatic
     class Article < Base
+      include Fastbill::Automatic::Services::Update
 
-      attr_reader :article_number, :title, :description, :unit_price, :allow_multiple, :is_addon, :currency_code,
-                  :vat_percent, :setup_fee, :subscription_interval, :subscription_number_events, :subscription_trail,
-                  :subscription_duration, :subscription_cancellation, :return_url_success, :return_url_cancel, :checkout_url
+      attr_accessor :article_number,
+                    :title,
+                    :description,
+                    :unit_price,
+                    :allow_multiple,
+                    :is_addon,
+                    :currency_code,
+                    :vat_percent,
+                    :setup_fee,
+                    :subscription_interval,
+                    :subscription_number_events,
+                    :subscription_trail,
+                    :subscription_duration,
+                    :subscription_cancellation,
+                    :return_url_success,
+                    :return_url_cancel,
+                    :checkout_url
 
       def self.create(attributes)
         raise FastbillError.new('Create method not implemented.')
